960b429a0f09f932f6677aad89382671eba62295
Divyanshi Mittal <divyanshimittal03@gmail.com>
2026-05-31 22:01:25 +0530

    removed one extra variable

--- Arrays/26_RemoveDupliocateFromSortedArray.java
+++ Arrays/26_RemoveDupliocateFromSortedArray.java
@@ -11,15 +11,14 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         
         int slow=0;
-        int k=1;
         for(int fast=1;fast<nums.length;fast++){
             if(nums[slow]!=nums[fast]){
                 slow++;
                 nums[slow]=nums[fast];
-                k++;
+                
             }
         }
 
-        return k;
+        return slow+1;
     }
 }
