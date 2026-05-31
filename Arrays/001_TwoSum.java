19fd90b884dcb2e4fda00f7d0e1c75352969a392
Divyanshi Mittal <divyanshimittal03@gmail.com>
2026-05-31 08:19:34 +0530

    Added TwoSum solutions

---
+++ Arrays/001_TwoSum.java
@@ -0,0 +1,24 @@
+//Approach:
+//Use HashMap to store visited numbers.
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
