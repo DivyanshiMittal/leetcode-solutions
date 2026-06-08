be620112ed705c719a48dec78c7699c3696001a3
Divyanshi Mittal <divyanshimittal03@gmail.com>
2026-06-08 23:18:02 +0530

    added time complexity

--- heap/215_KthLargestElementInAnArray.java
+++ heap/215_KthLargestElementInAnArray.java
@@ -1,6 +1,14 @@
+//Approach: 
+//We can use a min heap to keep track of the k largest elements in the array.
+//We iterate through the array and add each element to the min heap.
+//If the size of the min heap exceeds k, we remove the smallest element from the heap.
+//After processing all elements, the root of the min heap will be the kth largest element.
+
+//Time Complexity: O(n log k) - We iterate through all n elements and each insertion/deletion operation in the heap takes O(log k).
+//Space Complexity: O(k) - The heap will contain at most k elements.
+
+
 package heap;
-
-
 import java.util.*;
 
 class Solution {
